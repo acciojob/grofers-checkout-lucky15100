f4e652031708301b733c2031a1d262eeaed52718
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-21 22:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// Function to calculate the total price
+const calculateTotalPrice = () => {
+  const itemElements = document.querySelectorAll(".item");
+  const priceElements = document.querySelectorAll(".price");
+  let totalPrice = 0;
 
-const getSum = () => {
-//Add your code here
-  
+  // Loop through the item and price elements to calculate the total
+  for (let i = 0; i < itemElements.length; i++) {
+    const price = parseFloat(priceElements[i].textContent); // Parse price as a number
+    totalPrice += price;
+  }
+
+  return totalPrice;
 };
 
-getSumBtn.addEventListener("click", getSum);
+// Function to update the total price row in the table
+const updateTotalPriceRow = () => {
+  const table = document.querySelector("table");
+  const totalPrice = calculateTotalPrice();
 
+  // Create a new row for the total price
+  const totalRow = table.insertRow(-1);
+  const cell1 = totalRow.insertCell(0);
+  cell1.colSpan = 2;
+  cell1.textContent = `Total Price: Rs ${totalPrice}`;
+
+  // Update the table caption
+  const caption = table.querySelector("caption");
+  caption.textContent = "Monthly savings (Updated)";
+};
+
+const getSumBtn = document.createElement("button");
+getSumBtn.textContent = "Get Total Price";
+document.body.appendChild(getSumBtn);
+
+getSumBtn.addEventListener("click", () => {
+  updateTotalPriceRow();
+});
